42ec82ab33b723fea807086749781c9984265c4c
gracesubianto <gracesubianto@gmail.com>
2020-11-04 23:28:03 +1300

    refactored unessecary code

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-// const port = process.env.PORT || 5000;
-
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/HomePage.html'));
-// });
-
-// app.listen(port, () => console.log(`test listening on port ${port}!`));
-
 const express = require('express'); 
 const app = express();
 app.use(express.static('public'));
