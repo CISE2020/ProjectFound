5f2f758de68b8a6757715a55726139e9cab627ab
gracesubianto <gracesubianto@gmail.com>
2020-11-04 23:06:46 +1300

    added public pathway

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const port = process.env.PORT || 5000;
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/HomePage.html'));
+// });
+
+// app.listen(port, () => console.log(`test listening on port ${port}!`));
+
+const express = require('express'); 
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 5000;
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HomePage.html'));
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+     res.sendFile('HomePage.html', {root: __dirname + '/public/'});
 });
-
-app.listen(port, () => console.log(`test listening on port ${port}!`));
+app.listen(process.env.PORT || 5000)
